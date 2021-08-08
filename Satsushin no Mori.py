2bce84c135ee36f74c87b491cbc5c9e9d0dce9d6
itaizs <nahuelrey50@gmail.com>
2021-08-08 04:23:24 -0300

    Create Satsushin no Mori.py

---
+++ Satsushin no Mori.py
@@ -0,0 +1,346 @@
+label start:
+
+scene cielo
+
+"En el planeta, hay lugares extraños, que no conocemos y son totalmente desconocidos para toda la humanidad."
+
+"Lo desconocido da miedo, hasta que lo conoces y le dejas de temer."
+
+
+label habitacion_1:
+
+scene habitacion_d
+
+define k = ("Tú")
+
+play sound "alarma.ogg"
+
+pause 4.0
+
+     k "¿Ya es de día?"
+
+k "(Mierda, no he dormido nada)"
+
+"Me visto con el uniforme de mi escuela, hago el desayuno y el almuerzo."
+
+scene departamento_d
+
+k "Me voy, aunque no haya nadie en casa"
+
+"Nota: En japón es costumbre avisar cuando te vas de casa"
+
+k "(Desde que mi padre se fue de viaje de negocios, la casa ha estado muy solitaria)"
+
+"Salgo de mi casa y me dirijo a la escuela"
+
+scene calle_departamento_d
+
+"me pregunto que será de mi padre en el extranjero"
+
+"¿Tanto tiempo lleva fuera de Japon?"
+
+"Creo que hace unos 4 meses se fue, y me dejó a cargo de la casa"
+
+"Mes a mes me da el dinero para pagar los impuestos y un extra para mi y la comida"
+
+"Si que cambio despues de lo de mi madre"
+
+scene city_d with dissolve
+
+pause 2.0
+
+"Hoy si que hará calor"
+
+define desconocido =("¿?")
+
+desconocido "Heeeeeeeeeeey, Keeei"
+
+k "(¿Eh?)"
+
+show yuto_1 at center
+
+"Me giro y lo encuentro a Yuto, mi amigo de la infancia"
+
+k "Aah, Yuto, buen dia"
+
+define y = ("Yuto")
+
+y "Hola Kei"
+
+k "No he dormido nada suspira"
+
+y "Se nota en tu cara, por cierto, ¿tienes la tarea de literatura?"
+
+k "Si, te la doy pero cambiale algunas cosas, que no quiero problemas"
+
+y "Tan aplicado como siempre"
+
+k "Almenos dame las gracias... {p}Oye, ¿viste el caso de los desaparecidos?"
+
+y "¿Eso de que desaparecieron 9 estudiantes de secundaria?. Si, es el tema del momento en las redes sociales"
+
+k "¿No te da curiosidad?. No hay evidencia ni un posible paradero o de un posible culpable."
+
+y "Leí que tiene que ver con grupos religiosos oscuros, nombraron al satanismo"
+
+k "Mmm..."
+
+k "Oye, faltan 4 semanas para las vacaciones de verano"
+
+y "¡Se me ha olvidado por completo! ¿donde quieres ir?"
+
+k "Tengo pensado en ir a la playa"
+
+y "Eso es muy tipico, me decepcionas ¿te parece acampar en el bosque?"
+
+k "No, eso no, le temo al bosque"
+
+y "Vaya vaya... ¿Hay un gallina aquí?"
+
+k "Mierda no, un animal salvaje nos puede hacer daño o algo peor, y no solo un animal, un insecto, lo que sea, asi que no"
+
+y "Okay don asustadizo, ya veremos cuando llegue el momento"
+
+k "Oye, ¿qué dices que hagamos en estas 4 semanas que quedan?"
+
+y "Podemos ir a la sala arcade"
+
+k "No tengo tanto dinero, el viejo del lugar tiene demasiado caros las fichas para los videojuegos"
+
+y "Tienes razon... ¿Por qué le temes a lo desconocido?."
+
+k "Sinceramente, no lo sé, tampoco se si serán traumas del pasado."
+
+y "(Si, son traumas del pasado)"
+
+k "Pero bueno... no quiero terminar como mi madre"
+
+y "Kei... tienes una araña en la cara"
+
+k "¿Qué?"
+
+"Sin darme tiempo de esquivar, yuto me da un golpe con su palma en mi cara"
+
+y "Creo que la maté"
+
+k "Eres un hijo de puta, Yuto..."
+
+"Como duele, mierda, si será"
+
+y "Jejeje"
+
+y "(Bien, evité un momento incómodo)"
+
+"Mientras hablamos ya comenzamos a ver la torre de la escuela con el nombre ``Aoyama Gaukuin`` en letras gigantes."
+
+scene school entrance day
+
+"Por suerte llegamos temprano, unos diez minutos antes de que la clase comenzara"
+
+k "Oh, ya llegamos"
+
+scene park day with dissolve
+
+y "Pasame los apuntes"
+
+"Le entrego la carpeta con la tarea"
+
+k "Oye, ¿no hay alguien vigilandonos en la ventana del tercer piso?"
+
+y "¿Esa no es Kyouko Hashimoto?"
+
+k "¿La chica de tercero?"
+
+y "Si, pero ¿Qué hace mirándonos?"
+
+"Ignoré su pregunta y nos dirigimos a nuestro lugar en la clase"
+
+scene clase_d with dissolve
+
+show maestra at center
+
+"La profesora llega y explica un nuevo tema, pero yo me pierdo pensando en esa chica"
+
+"¿Kyouko Hashimoto eh? Es reconocida por su carácter frío y su belleza inmensa, tambien apodada como ``La luna de hielo´´. Pero ¿Me estaba mirando?"
+
+"Me estoy ilusionando, como siempre... pero un poco de ilusiones no hace daño"
+
+"Pero si que es raro... ella nunca ve a alguien de esa manera..."
+
+hide maestra
+
+pause 1.0
+
+show yuto_1 at left
+
+y "Te vi perdido en clase, ¿Pasó algo?"
+
+k "..."
+
+y "Heeeeeeey"
+
+k "..."
+
+y "Maldito"
+
+hide yuto_1
+
+show yuto_3 at right
+
+"Siento un ardor muy fuerte en la cara, cuando reacciono, entiendo la situación"
+
+"Yuto me golpeo de nuevo"
+
+k "Maldita sea, la segunda vez en el dia, si serás"
+
+y "No reaccionabas, te estaba hablando, ¿en que piensas? Maldición"
+
+k "En Kyouko Hashimoto"
+
+hide yuto_3
+
+show yuto_4 at right
+
+y "Me esperaba una razón mas seria"
+
+y "Y no me esperaba que seas tan sincero"
+   
+hide yuto_4
+
+show yuto_1 at center 
+
+k "Siempre soy directo"
+
+y "¿Tú dices?"
+
+k "CLARO QUE SI"
+
+y "¿Y qué sucede en el caso de Kyouko?"
+
+k "..."
+
+k "Espera... ¿y la profe?"
+
+y "Dijo que tenia un asunto urgente y se fue"
+
+k "Que raro, no es tipico de ella"
+   
+pause 1.0
+
+play sound "campana.ogg"
+
+pause 3.0
+
+k "Ya es hora del almuerzo"
+
+y "eso si que ha pasado rapido"
+
+"En la hora del almuerzo paseamos por la escuela y nos encontramos a alguien que me tuvo con la mente en el aire esta mañana"
+
+stop sound
+
+scene pasillo de escuela
+
+show yuto_1 at left
+
+k "Oye Yuto, para mi que los 9 desaparecidos tienen que tener algo raro detras, no puede ser una simple desaparicion"
+
+y "oye... no lo pienses, no te llenes la cabeza de algo que no te sirve de nada"
+
+pause 1.0
+
+show kyouko1 at center
+
+k "(¿Kyouko Hashimoto? Qué hace aquí la Luna de hielo)"
+
+y "Oye Kei, ¿entendiste algo de la clase?"
+
+"Le respondí a Yuto que no y Hashimoto pasó por nuestro lado sin decir nada"
+
+hide kyouko1
+
+k "Que nervios"
+
+y "No me digas que te enamoraste"
+
+k "¿Qué? ¡No!"
+
+y "¿Y por qué te pones nervioso?"
+
+k "... No lo se"
+
+define h = ("Kyouko Hashimoto")
+
+show kyouko_hablando at center
+
+h "Kei, acompañame"
+
+"La gente que estaba en el pasillo quedó en silencio, no es normal escuchar su voz, luego, comenzaron a susurrar"
+
+y "(Qué suceso tan raro, y le sucede a Kei)"
+
+y "(que pasa con este dia)"
+
+"Decido acompañarla y me lleva hasta la azotea, donde había personas almorzando, pero al ver a la Luna de Hielo quedan callados"
+
+scene azotea with dissolve
+
+show kyouko_hablando at right
+
+h "Seré breve, ¿Sabes algo de los 9 estudiantes desaparecidos?"
+
+k "No se sabe el paradero ni siquiera el culpable del caso, hay rumores en los que nombran a grupos religiosos oscuros"
+
+h "No creo que sea eso"
+
+"Luego de decir eso se queda callada pensando"
+
+k "Pero... ¿Por qué me preguntas a mi?"
+
+"Ella decide no responder y se va ain responder a mi pregunta"
+
+hide kyouko_hablando
+
+k "¿Qué sucede con ella? diablos"
+
+desconocido "Oye, acaso... no te acaba de hablar la ``Luna de Hielo´´"
+
+k "Si, lo acabas de ver, ¿No?"
+
+pause 2.0
+
+scene pasillo de escuela
+
+"Eso si que fue raro..."
+
+pause 1.0
+
+show yuto_3
+
+y "¿Qué magia usaste Kei? ¡Dilo!"
+
+k "Calmate Yuto, estoy confundido por lo que acaba de pasar"
+
+hide yuto_3
+
+show yuto_1
+
+y "No es algo que se vea todos los dias la verdad..."
+
+play sound "campana.ogg"
+
+k "Anda... vamos a clase, Yuto"
+
+y "Que rapido fue eso"
+
+pause 3.0
+
+stop sound
+
+scene clase_d
+
+show maestra at center 
+
+define maestra = ("Maestra")
+
+maestra "Lo siento, tuve que irme de urgencia por problemas personales"
